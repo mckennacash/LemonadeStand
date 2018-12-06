a6a3f1bc66213d804227d3d735a557895d3be2ab
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-12-06 08:54:36 -0500

    sloop

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int lemon,supples,sugar,cups,price,quantity,days;
+        double priceSugar,priceCups,priceLemons;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+System.out.println("Welcome to the profit calculator. please type whcih you would like to use?");
+    }
+}
