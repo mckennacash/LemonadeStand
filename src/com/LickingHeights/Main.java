4f904d4f43e1ce8fa0abd58396ba35c2f054af70
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-12-10 08:15:27 -0500

    of

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,10 +6,19 @@ public class Main {
 
     public static void main(String[] args)
     {
-        int lemon,supples,sugar,cups,price,quantity,days;
-        double priceSugar,priceCups,priceLemons;
+        int supply_1,supplies,supply_2,price,quantity,days,supply_3;
+        double priceSupply1,priceSupply2,priceSupply3;
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-System.out.println("Welcome to the profit calculator. please type whcih you would like to use?");
+System.out.println("Welcome to the profit calculator. How many supplies do you need for business?");
+        supplies = keyboard.nextInt();
+        System.out.println("How many days of sale do you want to run?");
+        days = keyboard.nextInt();
+        System.out.println("How much does supply 1 cost per item?");
+        priceSupply1 = keyboard.nextDouble();
+        System.out.println("Okay, now how much does supply 2 cost per item?");
+        priceSupply2 = keyboard.nextDouble();
+
+
     }
 }
